6862cec5878e97fd372cdd7f17b2376f47753d05
StXt <stas.domanskiy@gmail.com>
2020-04-06 16:52:10 +0300

    Removed unnecessary console.logs

--- getUnique.js
+++ getUnique.js
@@ -6,25 +6,22 @@ function getUnique(arr) {
   let result = [];
   
   
-    while(initialArr.length) {
-      let currentItemArr = initialArr.splice(0, 1);
-      console.log('currentItem', currentItemArr[0]);
-      let matchingPosition = initialArr.indexOf(currentItemArr[0]);
-      
-      if(!~matchingPosition) {
-        result.push(currentItemArr[0]);
-        console.log('initialArr w/o', initialArr);
-        continue;
-      }
+  while(initialArr.length) {
+    let currentItemArr = initialArr.splice(0, 1);
+    let matchingPosition = initialArr.indexOf(currentItemArr[0]);
+    
+    if(!~matchingPosition) {
+      result.push(currentItemArr[0]);
+      continue;
+    }
 
-      do {
-        initialArr.splice(matchingPosition, 1);
-        console.log('initialArr', initialArr);
-        matchingPosition = initialArr.indexOf(currentItemArr[0]);
-      } while(matchingPosition !== -1);       
+    do {
+      initialArr.splice(matchingPosition, 1);
+      matchingPosition = initialArr.indexOf(currentItemArr[0]);
+    } while(matchingPosition !== -1);       
   }
 
   return result;
 }
 
-getUnique(arr1);
+getUnique(arr2);
