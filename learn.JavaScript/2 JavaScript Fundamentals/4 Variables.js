e0ae4a169bd5abc8ba82647ad610b8325fc2a2bb
StXt <stas.domanskiy@gmail.com>
2020-04-06 17:01:04 +0300

    added learnJS folder

---
+++ learn.JavaScript/2 JavaScript Fundamentals/4 Variables.js
@@ -0,0 +1,15 @@
+/* Task 1 */
+
+let admin, name;
+
+name = "John";
+admin = name;
+
+alert(wadmin);
+
+/* Task 2 */
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+/* Task 3 - no coding */
