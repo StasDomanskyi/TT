5f33e089d50426f8a349c264a93915accec7e0af
StXt <stas.domanskiy@gmail.com>
2020-04-06 19:27:14 +0300

    added function tasks

---
+++ learn-javaScript/2-javaScript-fundamentals/14-functions/14-functions.js
@@ -0,0 +1,28 @@
+/* Task 1 - no coding */
+
+/* Task 2 */
+
+let age = 15;
+
+// Two variants of function needed in task
+
+/* function checkAge(age) {
+  return age > 18 ? true : confirm('Did parents allow you?');
+} */
+
+function checkAge(age) {
+  return age > 18 || confirm('Did parents allow you?');
+} 
+
+checkAge(age);
+
+/* Task 3 */
+
+function min(a, b) {
+  return a < b ? a : b;
+}
+
+min(2, 5);
+
+/* Task 4 in 4-task.html */
+
