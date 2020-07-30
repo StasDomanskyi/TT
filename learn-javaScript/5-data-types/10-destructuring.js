1b4378674b9232c20ebefe301ecb08cf8aa3ec9e
unknown <Stanislav Domanskyi>
2020-07-30 18:37:21 +0300

    S-20 solved destructuring tasks

--- learn-javaScript/5-data-types/10-destructuring.js
+++ learn-javaScript/5-data-types/10-destructuring.js
@@ -1,3 +1,29 @@
 /* Task 1 */
+let user = {
+  name: "John",
+  years: 30
+};
+
+let {name, years: age, isAdmin = false} = user;
 
 /* Task 2 */
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+topSalary(salaries);
+
+function topSalary(salaries) {
+  let topName, max;
+
+  for (let [key, value] of Object.entries(salaries)) {
+    if ( !max || max < value) {
+      max = value;
+      topName = key;
+    }
+  }
+
+  return max ? topName : null;
+}
